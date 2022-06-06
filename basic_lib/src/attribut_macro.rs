d9a7854147a9bf10d6a86db2512e53d898db3872
cracker8090 <xubeiping0930@gmail.com>
2022-06-06 18:02:40 +0800

    add proc_macro

---
+++ basic_lib/src/attribut_macro.rs
@@ -0,0 +1,11 @@
+// use macro_demo::*;
+
+// // macro converts struct S to struct H
+// #[my_custome_attribute]
+// struct S {}
+
+// #[test]
+// fn test_macro() {
+//     // due to macro we have struct H in scope
+//     let demo = H {};
+// }
--- basic_lib/src/lib.rs
+++ basic_lib/src/lib.rs
@@ -1,3 +1,15 @@
+// extern crate proc_macro;
+// use proc_macro::TokenStream;
+// use quote::quote;
+
+// // using proc_macro_attribute to declare an attribute like procedural macro
+// #[proc_macro_attribute]
+// // _metadata is argument provided to macro call and _input is code to which attribute like macro attaches
+// pub fn my_custome_attribute(_metadata: TokenStream, _input: TokenStream) -> TokenStream {
+//     // returing a simple TokenStream for Struct
+//     TokenStream::from(quote! {struct H{}})
+// }
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -9,6 +21,7 @@ mod tests {
 
 pub mod basic_data;
 pub mod my_control;
+pub mod my_error;
 pub mod my_hashmap;
 pub mod my_match;
 pub mod my_method;
@@ -17,4 +30,3 @@ pub mod my_panic;
 pub mod my_string;
 pub mod my_struct;
 pub mod my_variable;
-pub mod my_error;
